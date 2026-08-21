75d36b5174c06872cb09187aa10a35a3d3b01e0a
rastogivaibhav <44193373+rastogivaibhav@users.noreply.github.com>
2026-08-21 14:32:13 +0100

    PI4.5 A1.5.1 assemble pinned HypoKosh runtime

---
+++ pi45_a1/a15_native_runtime/src/hypokosh_runtime.cpp
@@ -0,0 +1,4 @@
+#include "hypokosh_runtime_frontier_part_1.inc"
+#include "hypokosh_runtime_frontier_part_2.inc"
+#include "hypokosh_runtime_frontier_part_3.inc"
+#include "hypokosh_runtime_frontier_part_4.inc"
